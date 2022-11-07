f24672820fd9722046b7cea517587e167323ce39
banjoman420 <bipulregmi@gmail.com>
2022-11-07 09:07:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os 
 import sys 
 import pandas as pd 
+import openpyxl
+import xlrd
 
 
 #variables 
 cols = [1,2,3]
 df = pd.read_excel("pokemonData.xlsx",usecols=cols)
-# print(df)
+print(df)
 
-for n in df:
-    print(n)
+
+
