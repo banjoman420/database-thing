2e43c8e2ad2c6138739a42f5c0d24de576b4db65
banjoman420 <bipulregmi@gmail.com>
2022-11-07 09:03:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import openpyxl
+import os 
+import sys 
+import pandas as pd 
 
 
+#variables 
+cols = [1,2,3]
+df = pd.read_excel("pokemonData.xlsx",usecols=cols)
+# print(df)
 
-
-
+for n in df:
+    print(n)
