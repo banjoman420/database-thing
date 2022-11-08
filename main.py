ff1d3c61667a19e161c455935b401f7cd8716628
banjoman420 <bipulregmi@gmail.com>
2022-11-08 09:59:33 -0700

    cool

--- main.py
+++ main.py
@@ -3,12 +3,31 @@ import sys
 import pandas as pd 
 import openpyxl
 import xlrd
+import json
+import csv
+loop = True
 
+#loading info from .txt file
+def loadSave():
+  file = open("save.txt", "r")
+  data = json.load(file)
+  file.close()
+  return data
+
+def saveInfo(fileToSave):
+  file = open("save.txt", "w")
+  json.dump(fileToSave, file)
+  file.close()
 
 #variables 
-cols = [1,2,3]
+cols = [1,2,3,11,12]
 df = pd.read_excel("pokemonData.xlsx",usecols=cols)
-print(df)
+
+
+
+
+
+
 
 
 
