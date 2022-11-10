4b0c148a51a00367187a7a8da59f8028afaff412
banjoman420 <bipulregmi@gmail.com>
2022-11-10 10:00:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,36 +10,19 @@ loop = True
 
 pd.set_option('display.max_rows', None)
 
-#loading info from .txt file
-def loadSave():
-  file = open("save.txt", "r")
-  data = json.load(file)
-  file.close()
-  return data
-
-def saveInfo(fileToSave):
-  file = open("save.txt", "w")
-  json.dump(fileToSave, file)
-  file.close()
-
-#variables 
-cols = [1,2,3,11,12]
-df = pd.read_excel("pokemonData.xlsx",usecols=cols)
-
-newDfnames = df['Name'].tolist()
-newDfType1 = df['Type 1'].tolist()
 
 
-# saveIndex = newDfnames.index('Rhydon')
-# print(saveIndex)
-# print(newDfType1[120])
+df = pd.read_excel("pokemonData.xlsx",)
+
+# def typeOfPokemon(peram1):
+#   output = df["Name"].where(df['Type 1' or 'Type 2' or 'Generation' or 'Legendary'] == peram1)
+#   print(output.dropna())
+# typeOfPokemon("Water")
 
 def typeOfPokemon(peram1):
-  output = df["Name"].where(df['Type 1' or 'Type 2' or 'Generation' or 'Legendary'] == peram1)
+  output = df.loc[(df["Name"].where(df['Type 1'] == peram1)) | (df["Name"].where(df['Type 2'] == peram1 ])]
   print(output.dropna())
 typeOfPokemon("Water")
-
-
 
 
 
